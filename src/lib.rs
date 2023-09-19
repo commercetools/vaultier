8056b01da2e6873024ccf60eaef2350ee19a7c95
Mathias Oertel <maoertel@users.noreply.github.com>
2023-09-19 17:28:27 +0200

    Allow login to vault instead of token injection and setting secrets (#3)
    * Allow login and setting secrets
    
    * Structure in features
    
    * Add `auth` feature to documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-//! Vaultier is a crate to read secrets from Hashicorp Vault.
+//! Vaultier is a crate to read from and write secrets to Hashicorp Vault.
 //!
 //!
-//! ``` compile_fail
+//! ```compile_fail
 //! use vaultier::SecretClient;
 //!
 //! #[derive(serde::Deserialize)]
@@ -13,7 +13,14 @@
 //! let address = "<vault instance address>";
 //! let mount = String::from("<mount>");
 //! let base_path = String::from("<base_path>");
+//!
+//! // With token or default feature enabled
 //! let client = SecretClient::new(address, mount, base_path, None).unwrap();
+//!
+//! // With auth feature enabled
+//! let auth_mount = "<mount to vault auth>";
+//! let role = "<your role>";
+//! let client = SecretClient::create(address, auth_mount, role, mount, base_path).unwrap();
 //!
 //! // read secrets from that base path
 //! let secrets = client.read_secrets::<MySecrets>().await.unwrap();
@@ -22,6 +29,8 @@
 //! let secrets = client.read_secrets_from::<MySecrets>("my-secrets").await.unwrap();
 //! ```
 
+#[cfg(feature = "auth")]
+mod auth;
 pub mod error;
 
 use std::fs::File;
@@ -31,9 +40,18 @@ use serde::Deserialize;
 use vaultrs::client::{VaultClient, VaultClientSettingsBuilder};
 use vaultrs::kv2;
 
+#[cfg(feature = "write")]
+use serde::Serialize;
+#[cfg(feature = "write")]
+use vaultrs::api::kv2::responses::SecretVersionMetadata;
+
 use crate::error::Result;
 
-const TOKEN_PATH: &str = "/vault/secrets/token";
+#[cfg(feature = "auth")]
+use crate::auth::login;
+
+#[cfg(feature = "token")]
+const VAULT_TOKEN_PATH: &str = "/vault/secrets/token";
 
 /// A client to read secrets from Hashicorp Vault.
 ///
@@ -47,12 +65,13 @@ pub struct SecretClient {
 }
 
 impl SecretClient {
-    /// Convenience method to create a new SecretClient.
+    /// Convenience method to create a new SecretClient from a vault token.
     ///
     /// - address is the address of your Vault instance.
     /// - mount is the mount point of the KV2 secrets engine.
     /// - base_path reflects the lowest level of where secrets are located
     /// - token is the Vault token to use. If no token is passed it tries to read the token from /vault/secrets/token.
+    #[cfg(feature = "token")]
     pub fn new(
         address: &str,
         mount: String,
@@ -61,9 +80,38 @@ impl SecretClient {
     ) -> Result<SecretClient> {
         let token = match token {
             Some(token) => token,
-            None => read_vault_token(TOKEN_PATH)?,
+            None => read_token_from(VAULT_TOKEN_PATH)?,
         };
 
+        Self::create_internal(address, mount, base_path, &token)
+    }
+
+    /// Convenience method to create a new SecretClient with a login to vault.
+    ///
+    /// - address is the address of your Vault instance
+    /// - auth_mount is the mount path of the vault authentication
+    /// - role is the vault role to use for the login
+    /// - mount is the mount point of the KV2 secrets engine
+    /// - base_path reflects the lowest level of where secrets are located
+    #[cfg(feature = "auth")]
+    pub async fn create(
+        address: &str,
+        auth_mount: &str,
+        role: &str,
+        mount: String,
+        base_path: String,
+    ) -> Result<SecretClient> {
+        let auth = login(address, auth_mount, role).await?;
+
+        Self::create_internal(address, mount, base_path, &auth.client_token)
+    }
+
+    fn create_internal(
+        address: &str,
+        mount: String,
+        base_path: String,
+        token: &str,
+    ) -> Result<SecretClient> {
         let client = VaultClient::new(
             VaultClientSettingsBuilder::default()
                 .address(address)
@@ -74,7 +122,17 @@ impl SecretClient {
         Ok(SecretClient { client, mount, base_path })
     }
 
+    /// Read secrets from the base path.
+    #[cfg(feature = "read")]
+    pub async fn read_secrets<A>(&self) -> Result<A>
+    where
+        A: for<'de> Deserialize<'de>,
+    {
+        self.read_secrets_internal::<A>(&self.base_path).await
+    }
+
     /// Read secrets from the passed path relative to the base path.
+    #[cfg(feature = "read")]
     pub async fn read_secrets_from<A>(&self, path: &str) -> Result<A>
     where
         A: for<'de> Deserialize<'de>,
@@ -83,14 +141,7 @@ impl SecretClient {
         self.read_secrets_internal::<A>(&path).await
     }
 
-    /// Read secrets from the base path.
-    pub async fn read_secrets<A>(&self) -> Result<A>
-    where
-        A: for<'de> Deserialize<'de>,
-    {
-        self.read_secrets_internal::<A>(&self.base_path).await
-    }
-
+    #[cfg(feature = "read")]
     async fn read_secrets_internal<A>(&self, path: &str) -> Result<A>
     where
         A: for<'de> Deserialize<'de>,
@@ -98,9 +149,41 @@ impl SecretClient {
         let secrets: A = kv2::read(&self.client, &self.mount, path).await?;
         Ok(secrets)
     }
+
+    /// Set secrets in the base path.
+    #[cfg(feature = "write")]
+    pub async fn set_secrets<A>(&self, data: &A) -> Result<SecretVersionMetadata>
+    where
+        A: Serialize,
+    {
+        self.set_secrets_internal(&self.base_path, data).await
+    }
+
+    /// Set secrets in the base path.
+    #[cfg(feature = "write")]
+    pub async fn set_secrets_in<A>(&self, path: &str, data: &A) -> Result<SecretVersionMetadata>
+    where
+        A: Serialize,
+    {
+        let path = format!("{}/{}", self.base_path, path);
+        self.set_secrets_internal(&path, data).await
+    }
+
+    #[cfg(feature = "write")]
+    pub async fn set_secrets_internal<A>(
+        &self,
+        path: &str,
+        data: &A,
+    ) -> Result<SecretVersionMetadata>
+    where
+        A: Serialize,
+    {
+        let auth_info = kv2::set(&self.client, &self.mount, path, data).await?;
+        Ok(auth_info)
+    }
 }
 
-fn read_vault_token(path: &str) -> Result<String> {
+fn read_token_from(path: &str) -> Result<String> {
     let mut file = File::open(path)?;
     let mut token = String::new();
     file.read_to_string(&mut token)?;
